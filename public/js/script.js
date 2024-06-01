50e5ea91c58eabb6f4305460663ead34c3bf26ab
NsikanPatrick <nsikanpatrick69@gmail.com>
2024-06-01 17:06:28 +0200

    Improving the navbar

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,13 @@
+const body = document.querySelector("body");
+const navigation = document.querySelector(".navigation");
 const menuBtn = document.querySelector('.menu-btn');
 const menuItems = document.querySelector('.menu-items');
 
 menuBtn.addEventListener('click', () => {
     menuItems.classList.toggle('open');
+    body.classList.toggle("disabledScroll");
 })
+
+window.onscroll = () => {
+    this.scrollY > 20 ? navigation.classList.add("sticky") : navigation.classList.remove("sticky");
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: { /** Extend tailwind's colors and fonts here */
+      colors:{
+        primary:{
+          50: '#250343',
+          100: '#140864', /**rgb(20,8,100),rgb(64,31,173),rgb(103,65,222),rgb(167,116,227),rgb(91,54,136) */
+          200: '#401fad',
+          300: '#6741de',
+          400: '#1B0D82',
+        },
+        secondary:{
+          50: '#a774e3',
+          100: '#5b3688',
+          200: '#D06CC6',
+        }
+      }
+    },
   },
   plugins: [],
 }
